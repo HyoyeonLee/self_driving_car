aa961b5a29322749a0be025e61a5ede063307ddb
HYOYEON LEE <hyoyeon.y.lee@gmail.com>
2021-10-06 15:35:57 +0900

    progress

--- defs.py
+++ defs.py
@@ -1,9 +1,8 @@
 import params
 import RPi.GPIO as GPIO
 import time
-
-
-
+import serial
+import threading
 
 """
 ********************************************************
@@ -20,16 +19,26 @@ pwm_motor_left = GPIO.PWM(MOTOR_A_L_PWM,500)
 pwm_motor_right = GPIO.PWM(MOTOR_B_R_PWM,500)
 pwm_motor_left.start(0)
 pwm_motor_right.start(0)
-
 pwm_buzzer = GPIO.PWM(BUZZER,nDo)
 pwm_buzzer.stop()
-
+#find serial port of bluetooth
+#by      :  ls -l /dev/serial0
+#returns :  >ttyS0
+bt_serial = serial.Serial("/dev/ttyS0",baudrate = 9600, timeout = 1.0)
 
 """
 ********************************************************
-       Functions 
+       Global variabile & Functions 
 ********************************************************
 """
+grx_data = ""
+def bt_serial_thread():
+    global grx_data 
+    while True:
+        rx_data = bt_serial.readline()
+        rx_data = rx_data.decode()
+        grx_data = rx_data
+
 def go_forward(speed):
     for i in range(0,4):
         GPIO.output(MOTOR_INs[i],STATE_FORWARD[0][i])
@@ -43,7 +52,6 @@ def go_backward(speed):
         GPIO.output(LEDs[i],STATE_BACKWARD[1][i])
     pwm_motor_left.changeDutyCycle(speed)
     pwm_motor_right.changeDutyCycle(speed)
-
 
 def go_left(speed):
     for i in range(0,4):
@@ -71,17 +79,39 @@ def go_stop(speed):
        MAIN() 
 ********************************************************
 """
-go_where = [go_forward,go_backward,go_left,go_right,go_stop]
-speed = 50
-try:
-    while True:
-        for i in range(0,4):
-            if GPIO.input(i)==1:
-                go_where[i](speed)
-except: KeyboardInterrupt:
-    pass
-go_where[4](0)
-GPIO.cleanup()
+def main():
+    global grx_data
+    cmd = ["go","back","left","right","stop"]
+    go_where = [go_forward,go_backward,go_left,go_right,go_stop]
+    speed = 50
+    try:
+        while True:
+            for i in range(0,5):
+                if grx_data.find(cmd[i])>=0:
+                    grx_data = ""
+                    print("OK! %s",cmd[i])
+                    if(cmd[i]=="stop"):
+                        speed = 0
+                    go_where[i](speed)
+            for i in range(0,4):
+                if(GPIO.input(SWs[i]==1):
+                        print("Emergency Stop!!")
+                        go_where[-1](0)
+    except: KeyboardInterrupt:
+        pass
+
+"""
+********************************************************
+       Jobs 
+********************************************************
+"""
+if __name__=="__main__":
+    task1 = threading.Thread(target = bt_serial_thread)
+    task1.start()
+    main()
+    bt_serial.close()
+    GPIO.cleanup()
+
 
 
 
